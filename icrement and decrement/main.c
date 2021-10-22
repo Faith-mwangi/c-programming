20f17b73be6085ac25683f70627a6f0847706874
Faith-mwangi <91890451+Faith-mwangi@users.noreply.github.com>
2021-10-22 13:21:05 +0300

    Add files via upload

---
+++ icrement and decrement/main.c
@@ -0,0 +1,15 @@
+
+// Working of increment and decrement operators
+#include <stdio.h>
+int main()
+{
+    int a = 5, b = 60;
+    float c = 4, d = 100;
+
+    printf("++a = %d \n", ++a);
+    printf("--b = %d \n", --b);
+    printf("++c = %f \n", ++c);
+    printf("--d = %f \n", --d);
+
+    return 0;
+}
