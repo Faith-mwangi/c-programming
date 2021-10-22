f1dbf3658a63498ca8b37e45f832c89712d17677
Faith-mwangi <91890451+Faith-mwangi@users.noreply.github.com>
2021-10-22 13:20:37 +0300

    Add files via upload

---
+++ relational/main.c
@@ -0,0 +1,21 @@
+// Working of relational operators
+#include <stdio.h>
+int main()
+{
+    int a = 3, b = 4, c = 12;
+
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+
+    return 0;
+}
