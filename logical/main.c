59b5f4344cee7e38c068539dea3042e002bb786e
Faith-mwangi <91890451+Faith-mwangi@users.noreply.github.com>
2021-10-22 13:20:52 +0300

    Add files via upload

---
+++ logical/main.c
@@ -0,0 +1,27 @@
+// Working of logical operators
+
+#include <stdio.h>
+int main()
+{
+    int a = 4, b = 2, c = 8, result;
+
+    result = (a == b) && (c > b);
+    printf("(a == b) && (c > b) is %d \n", result);
+
+    result = (a == b) && (c < b);
+    printf("(a == b) && (c < b) is %d \n", result);
+
+    result = (a == b) || (c < b);
+    printf("(a == b) || (c < b) is %d \n", result);
+
+    result = (a != b) || (c < b);
+    printf("(a != b) || (c < b) is %d \n", result);
+
+    result = !(a != b);
+    printf("!(a != b) is %d \n", result);
+
+    result = !(a == b);
+    printf("!(a == b) is %d \n", result);
+
+    return 0;
+}
