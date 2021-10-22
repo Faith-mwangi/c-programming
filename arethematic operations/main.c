413a4bb4ae894dfe9abef9473dbb2ffee57e6593
Faith-mwangi <91890451+Faith-mwangi@users.noreply.github.com>
2021-10-22 13:21:53 +0300

    Add files via upload

---
+++ arethematic operations/main.c
@@ -0,0 +1,19 @@
+/*arithmetic operations*/
+#include <stdio.h>
+int main()
+{
+    int a,b,add,subtract,divide,multipy,reminder;
+    printf("enter a,b values");
+    scanf("%d%d",&a,&d);
+    add=a+b;
+    subtract=a-b;
+    divide=a/b;
+    multiply=a*b
+    reminder=a%b;
+    printf("results of add is=%d/n",add);
+    printf("results of subtract is=%d/n",subtract);
+    printf("results of divide is=%d/n",divide);
+    printf("results of multiply is=%d/n",multiply);
+    printf("results of reminder is=%d/n",reminder);
+    return 0;
+}
